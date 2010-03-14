503a0868eaf8789fa0bd7a605993f9c0bf0da4f8
Anis BEREJEB <anis.berejeb@gmail.com>
2010-03-13 20:26:05 -0500

    Added cache and dependency injection container

---
+++ core/api/cache/Cache.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Note : Code is released under the GNU LGPL
+ *
+ * Please do not change the header of this file 
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * Lesser General Public License as published by the Free Software Foundation; either version 2 of 
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * See the GNU Lesser General Public License for more details.
+ */
+
+/**
+ * File:        Cache.php
+ * 
+ * @author      Anis BEREJEB
+ * 
+ */
+
+/**
+ * Implements an array cache
+ */
+abstract class Cache implements ICache 
+{
+    /**
+     * prefix
+     * 
+     * @var mixed  Defaults to ''. 
+     */
+    protected $prefix = '';
+    /**
+     * prefix setter
+     * 
+     * @param  string  $prefix the prefix for all cache keys
+     * @Exception if prefix is empty
+     */
+    public function setPrefix($prefix)
+    {
+        if (empty($prefix))
+        {
+            throw new Exception('prefix should not be empty.');
+        }    
+        $this->prefix = $prefix;
+    } 
+    
+    /**
+     * Prefix setter
+     * 
+     * @return string the cache prefix
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }    
+}

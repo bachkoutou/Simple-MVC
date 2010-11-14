aca423f127a376d2d4c5c69b0e65fcdca7020905
Anis BEREJEB <anis.berejeb@gmail.com>
2010-11-14 12:10:59 -0500

    Added ttl support for FileCache

--- core/api/cache/FileCache.php
+++ core/api/cache/FileCache.php
@@ -26,6 +26,12 @@
  */
 class FileCache extends Cache 
 {
+    // the length of the TTL into the cache file
+    const TTL_LENGTH = 10;
+
+    // The string that prepend the ttl value into the cache file
+    const TTL_PREPEND_STRING = 'TTL=';
+
     /**
      * the cache handler
      * 
@@ -42,22 +48,49 @@ class FileCache extends Cache
      */
     public function __construct($directory = null)
     {
-        parent::__construct();
         $this->directory = (!empty($directory)) ?  $directory : sys_get_temp_dir() . DIRECTORY_SEPARATOR;
     }
+    
+    /**
+     * Directory setter
+     * 
+     * @param  string  $directory 
+     */
+    public function setDirectory($directory)
+    {
+        if (!is_dir($directory))
+        {
+            throw new InvalidArgumentException('directory ' . $directory . ' is not a directory');
+        }    
+        if (!is_writable($directory))
+        {
+            throw new InvalidArgumentException('directory ' . $directory . ' is not writeable');
+        }    
+        $this->directory = $directory;
+    }    
 
+    /**
+     * Directory Getter
+     * 
+     * @return string $directory
+     */
+    public function getDirectory()
+    {
+        return $this->directory;
+    }
     /**
      * Sets an element in the cache
      * 
      * @param  mixed  $offset The offset
      * @param  mixed   $value  The value
-     * @param int $ttl Time to live - NOT IMPLEMENTED
+     * @param int $ttl Time to live
      */
     public function set($offset, $value, $ttl = null) 
     {
-        file_put_contents($this->directory . md5($this->prefix) . '_' . md5($offset), $value);
+        $ttl = self::TTL_PREPEND_STRING . str_pad((empty($ttl) ? 0 : (int)$ttl), self::TTL_LENGTH, ' ', STR_PAD_RIGHT);
+        file_put_contents($this->directory . $this->prefix . '_' . md5($offset), $ttl . $value);
     }
-    
+
     /**
      * Checks if an offset exists 
      * 
@@ -66,8 +99,32 @@ class FileCache extends Cache
      */
     public function exists($offset) 
     {
-        return is_file($this->directory . md5($this->prefix) . '_' . md5($offset));
+        $filePath = $this->directory . $this->prefix . '_' . md5($offset);
+        if (is_file($filePath))
+        {
+            $cacheContent = file_get_contents($filePath);
+
+            $ttl = (int)rtrim(substr($cacheContent, strlen(self::TTL_PREPEND_STRING), self::TTL_LENGTH));
+            // ttl of 0 = unlimited cache time.
+            if (0 === $ttl)
+            {
+                return substr($cacheContent, (self::TTL_LENGTH + strlen(self::TTL_PREPEND_STRING)));
+            }
+
+            // get the cache file creation time
+            $fileTimestamp = filectime($filePath);
+            // if the cache file creation time + the ttl still lower than the current time, we return the cache content.
+            // otherwise, the cache content is expired
+            if ($fileTimestamp + $ttl > time())
+            {
+                return substr($cacheContent, (self::TTL_LENGTH + strlen(self::TTL_PREPEND_STRING)));
+            }
+            // cache content TTL expired, let's delete it from the cache
+            $this->delete($offset);
+        }
+        return false;
     }
+
     
     /**
      * Deletes a value from the cache
@@ -76,9 +133,9 @@ class FileCache extends Cache
      */
     public function delete($offset) 
     {
-        return unlink($this->directory . md5($this->prefix) . '_' . md5($offset));
+        return unlink($this->directory . $this->prefix . '_' . md5($offset));
     }
-    
+
     /**
      * Gets a value from the cache
      * 
@@ -87,7 +144,7 @@ class FileCache extends Cache
      */
     public function get($offset) 
     {
-        return file_get_contents($this->directory . md5($this->prefix) . '_' . md5($offset));
+        return substr(file_get_contents($this->directory . $this->prefix . '_' . md5($offset)), (self::TTL_LENGTH + strlen(self::TTL_PREPEND_STRING)));
     }
 
     /**
@@ -95,9 +152,9 @@ class FileCache extends Cache
      */
     public function clear()
     {
-        foreach (glob($this->directory . md5($this->prefix) . "/*") as $file)
+        foreach (glob($this->directory . $this->prefix . "/*") as $file)
         {
             unlink($file);
         }    
-    }    
-}
+    }   
+}    
